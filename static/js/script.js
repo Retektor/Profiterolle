8afae62a3d384005f51920eb489c4c62fcfb4091
Retektor <zhenya.volokitin@list.ru>
2025-10-14 21:11:04 +0400

    Products carousel
    Added products carousel with placeholders through jQuery + Slick

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.products_carousel').slick({
+        dots: true,           // Show dot indicators
+        arrows: true,         // Show next/prev arrows
+        infinite: true,       // Infinite looping
+        speed: 300,           // Transition speed
+        slidesToShow: 3,      // Number of slides to show
+        slidesToScroll: 1,    // Number of slides to scroll
+        autoplay: true,       // Auto-play
+        autoplaySpeed: 2000,  // Auto-play speed
+    });
+});
