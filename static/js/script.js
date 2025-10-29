f733e3e5ff1207f08a44a894936992aec14fdde7
Retektor <zhenya.volokitin@list.ru>
2025-10-29 20:33:08 +0400

    Adaptive website progress

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,21 @@
 $(document).ready(function(){
     $('.products_carousel').slick({
-        dots: true,
-        arrows: true,
-        infinite: true,
-        speed: 300,
         slidesToShow: 3,
         slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        arrows: true,
         autoplay: true,
         autoplaySpeed: 3000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                }
+            }
+        ]
     });
 });
